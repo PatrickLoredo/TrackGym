44ebd9c9bb591f5c69746312fecb67a2d9dedb5d
Patrick Loredo <phlsjw@gmail.com>
2026-09-22 10:11:24 -0300

    -

--- js/meuTreino.js
+++ js/meuTreino.js
@@ -1688,7 +1688,7 @@ function exibeExerciciosFichaAberta(idCampo, indiceFicha, indiceTreino) {
 
                                 <!-- BOTÃO DO CHECK EXERCICIO -->
                                 <button type="button" 
-                                class="btn btn-danger input-group-text d-none w-100 p-2"
+                                class="btn btn-danger d-block w-100 p-2"
                                  id="ThumbsX_${idChevron}">
                                     <i class="fa fa-x"
                                     onclick="checaExercicio('${idNome}',
