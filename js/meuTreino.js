e8193d0c701d92a6aebd5a0219677302b26a12e0
Patrick Loredo <phlsjw@gmail.com>
2026-09-22 14:05:39 -0300

    -

--- js/meuTreino.js
+++ js/meuTreino.js
@@ -1761,11 +1761,11 @@ function exibeExerciciosFichaAberta(idCampo, indiceFicha, indiceTreino) {
                                 </div>
                                 <div class="row mt-2 mb-3">
                                     <div class="col">
-                                        <div class="input-group">
-                                            <button class="input-group-text uppercase tamanho07 btn btn-primary input-group-text"
+                                        <div class="input-group flexCenter">
+                                            <button class="input-group-text uppercase tamanho08 btn btn-primary input-group-text"
                                             onclick="aumentarCarga('${idCarga}', 5)">+5</button>
 
-                                            <button class="input-group-text uppercase tamanho07 btn btn-primary input-group-text"
+                                            <button class="input-group-text uppercase tamanho08 btn btn-info input-group-text"
                                             onclick="aumentarCarga('${idCarga}', 7)">+7</button>
                                         </div>
                                     </div>
